0d5de25ef825be96ad4d6a0538645cb2d0bc3304
Matti Airas <matti.airas@hatlabs.fi>
2024-10-10 18:59:12 +0300

    Update main.cpp to reflect changes in SensESP

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 // Remove the parts that are not relevant to you, and add your own code
 // for external hardware libraries.
 
+#include <memory>
+
+#include "sensesp.h"
 #include "sensesp/sensors/analog_input.h"
 #include "sensesp/sensors/digital_input.h"
 #include "sensesp/sensors/sensor.h"
@@ -19,7 +22,7 @@ using namespace sensesp;
 
 // The setup function performs one-time application initialization.
 void setup() {
-  SetupLogging();
+  SetupLogging(ESP_LOG_DEBUG);
 
   // Construct the global SensESPApp() object
   SensESPAppBuilder builder;
@@ -28,7 +31,8 @@ void setup() {
                     ->set_hostname("my-sensesp-project")
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
-                    //->set_wifi("My WiFi SSID", "my_wifi_password")
+                    //->set_wifi_client("My WiFi SSID", "my_wifi_password")
+                    //->set_wifi_access_point("My AP SSID", "my_ap_password")
                     //->set_sk_server("192.168.10.3", 80)
                     ->get_app();
 
@@ -42,7 +46,7 @@ void setup() {
 
   // Create a new Analog Input Sensor that reads an analog input pin
   // periodically.
-  auto* analog_input = new AnalogInput(
+  auto analog_input = std::make_shared<AnalogInput>(
       kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
 
   // Add an observer that prints out the current value of the analog input
@@ -56,16 +60,15 @@ void setup() {
   const uint8_t kDigitalOutputPin = 15;
   const unsigned int kDigitalOutputInterval = 650;
   pinMode(kDigitalOutputPin, OUTPUT);
-  SensESPBaseApp::get_event_loop()->onRepeat(
-      kDigitalOutputInterval, [kDigitalOutputPin]() {
-        digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
-      });
+  event_loop()->onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
+    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
+  });
 
   // Read GPIO 14 every time it changes
 
   const uint8_t kDigitalInput1Pin = 14;
-  auto* digital_input1 =
-      new DigitalInputChange(kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
+  auto digital_input1 = std::make_shared<DigitalInputChange>(
+      kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
 
   // Connect the digital input to a lambda consumer that prints out the
   // value every time it changes.
@@ -73,8 +76,10 @@ void setup() {
   // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
   // see if the value changes!
 
-  digital_input1->connect_to(new LambdaConsumer<bool>(
-      [](bool input) { debugD("Digital input value changed: %d", input); }));
+  auto digital_input1_consumer = std::make_shared<LambdaConsumer<bool>>(
+      [](bool input) { debugD("Digital input value changed: %d", input); });
+
+  digital_input1->connect_to(digital_input1_consumer);
 
   // Create another digital input, this time with RepeatSensor. This approach
   // can be used to connect external sensor library to SensESP!
@@ -93,28 +98,47 @@ void setup() {
   // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
   // wire and see if the value changes!
 
-  auto* digital_input2 = new RepeatSensor<bool>(
+  auto digital_input2 = std::make_shared<RepeatSensor<bool>>(
       kDigitalInput2Interval,
       [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
 
   // Connect the analog input to Signal K output. This will publish the
   // analog input value to the Signal K server every time it changes.
-  analog_input->connect_to(new SKOutputFloat(
-      "sensors.analog_input.voltage",         // Signal K path
-      "/Sensors/Analog Input/Voltage",        // configuration path, used in the
-                                              // web UI and for storing the
-                                              // configuration
-      new SKMetadata("V",                     // Define output units
-                     "Analog input voltage")  // Value description
-      ));
+  auto aiv_metadata = std::make_shared<SKMetadata>("V", "Analog input voltage");
+  auto aiv_sk_output = std::make_shared<SKOutput<float>>(
+      "sensors.analog_input.voltage",   // Signal K path
+      "/Sensors/Analog Input/Voltage",  // configuration path, used in the
+                                        // web UI and for storing the
+                                        // configuration
+      aiv_metadata
+  );
+
+  ConfigItem(aiv_sk_output)
+      ->set_title("Analog Input Voltage SK Output Path")
+      ->set_description("The SK path to publish the analog input voltage")
+      ->set_sort_order(100);
+
+  analog_input->connect_to(aiv_sk_output);
 
   // Connect digital input 2 to Signal K output.
-  digital_input2->connect_to(new SKOutputBool(
-      "sensors.digital_input2.value",          // Signal K path
-      "/Sensors/Digital Input 2/Value",        // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Digital input 2 value")  // Value description
-      ));
+  auto di2_metadata = std::make_shared<SKMetadata>("", "Digital input 2 value");
+  auto di2_sk_output = std::make_shared<SKOutput<bool>>(
+      "sensors.digital_input2.value",    // Signal K path
+      "/Sensors/Digital Input 2/Value",  // configuration path
+      di2_metadata
+  );
+
+  ConfigItem(di2_sk_output)
+      ->set_title("Digital Input 2 SK Output Path")
+      ->set_sort_order(200);
+
+  digital_input2->connect_to(di2_sk_output);
+
+  // To avoid garbage collecting all shared pointers created in setup(),
+  // loop from here.
+  while (true) {
+    loop();
+  }
 }
 
-void loop() { SensESPBaseApp::get_event_loop()->tick(); }
+void loop() { event_loop()->tick(); }
